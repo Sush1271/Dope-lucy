662202ede8b5ebe8dc5d95309606eb534974ed8f
Sush1271 <warzonesushant@gmail.com>
2026-09-13 23:35:29 +0530

    hyprland

---
+++ support/hypr/modules/monitors.lua
@@ -0,0 +1,19 @@
+------------------
+---- MONITORS ----
+------------------
+
+
+
+hl.monitor({
+    output   = "HDMI-A-1",
+    mode     = "preferred",
+    position = "0x0",
+    scale    = "1",
+})
+
+hl.monitor({
+    output   = "DP-1",
+    mode     = "preferred",
+    position = "1920x0",
+    scale    = "1",
+})
